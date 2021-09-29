be979a7e93767599ba4a779bc3847f59e54a2e85
Aldo Cesar <57098697+Aldo-lima@users.noreply.github.com>
2021-09-29 17:24:58 -0300

    mascara form fornecedor

--- fornecedor/forms.py
+++ fornecedor/forms.py
@@ -6,3 +6,14 @@ class ForncedorForm(forms.ModelForm):
     class Meta:
         model = Fornecedor
         fields = '__all__'
+
+    #def __init__(self, *args, **kwargs):
+    #  super().__init__(self, *args, **kwargs)
+    #   self.fields['CNPJ'].widget.attrs.update({'class': 'CNPJ'})
+     #  self.fields['cep'].widget.attrs.update({'class': 'cep'})
+
+
+
+
+
+
